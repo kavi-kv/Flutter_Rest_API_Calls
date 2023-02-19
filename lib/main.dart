11d1bdf8664ca80f7063fb89c68f47747c87c0b3
Kavi <kavialin23@gmail.com>
2023-02-19 19:39:38 +0300

    19//2/2023

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:random_user_api_calls/Screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
